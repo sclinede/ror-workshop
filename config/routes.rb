f4026a8596c6eb1b0098bd9b8ca0091bc75268ad
Sergey D <sclinede@gmail.com>
2015-11-12 22:25:03 +0500

    Fix presentation layer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  get 'reports/subjects', to: 'reports#subjects', as: :report_subjects
+
+  resources :teachers do
+    get :subjects
+  end
+
   resources :students do
     get :subjects
   end
+
+  root to: "visitors#index"
 end
